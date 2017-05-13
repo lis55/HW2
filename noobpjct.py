dc026c2653650aa6615f567ed68b6a06fb0d42a2
lis55 <lis_lou77@msn.com>
2017-05-13 17:38:46 +0200

    Add files via upload

--- noobpjct.py
+++ noobpjct.py
@@ -14,9 +14,9 @@ import matplotlib.pyplot as plt
 
 N=60
 steps=5000
-Lred=math.pow(N/0.5,1/3)
+Lred=math.pow(2*N,1/3)
 Ared=0.8
-delta=1/4
+delta=1.0/4.0
 def distance(xi,xii,yi,yii,zi,zii):
     sq1 = xi-xii-Lred*math.floor((xi-xii)/Lred+0.5)
     sq2 = yi-yii-Lred*math.floor((yi-yii)/Lred+0.5)
@@ -42,67 +42,66 @@ for i in range(N):
         if i<j:
             dij=distance(r[i,0],r[j,0],r[i,1],r[j,1],r[i,2],r[j,2])
             if dij<1:
-                Etemp=math.pow(dij,3)
+                Etemp=1/math.pow(dij,3)
             if dij<Lred/2:
-                Etemp=math.pow(dij,2)*math.exp(-dij+1)
+                Etemp=1/math.pow(dij,2)*math.exp(-dij+1)
         E=E+Etemp
 E=Ared/N*E
 
 accep=0
 Ener=np.zeros(steps)
 for s in range(0,steps):
-    u=ran.randint(0,N-1)
-    xnew=r[u,0]+delta*(ran.random()-0.5)
-    ynew=r[u,1]+delta*(ran.random()-0.5)
-    znew=r[u,2]+delta*(ran.random()-0.5)
+    for u in range(N):
+        '''u=ran.randint(0,N-1)'''
+        xnew=r[u,0]+delta*(ran.random()-0.5)
+        ynew=r[u,1]+delta*(ran.random()-0.5)
+        znew=r[u,2]+delta*(ran.random()-0.5)
+        
+        Ener[s]=E
     
-    Ener[s]=E
-
-    Eij=0
-    for j in range(N):
-        Etemp=0
-        if j!=u:
-            dij=distance(r[u,0],r[j,0],r[u,1],r[j,1],r[u,2],r[j,2])
-            if dij<1:
-                Etemp=math.pow(dij,3)
-            if dij<Lred/2:
-                Etemp=math.pow(dij,2)*math.exp(-dij+1)
-        Eij=Eij+Etemp
-    Eij=Ared/N*Eij
-    
-    Eijnew=0
-    for j in range(N):
-        Etemp=0
-        if j!=u:
-            dij=distance(xnew,r[j,0],ynew,r[j,1],znew,r[j,2])
-            if dij<1:
-                Etemp=math.pow(dij,3)
-            if dij<Lred/2:
-                Etemp=math.pow(dij,2)*math.exp(-dij+1)
-        Eijnew=Eijnew+Etemp
-    Eijnew=Ared/N*Eijnew
-    
-    Enew=E-Eij+Eijnew
-    D_E=Eijnew-Eij
-    
-    if D_E<=0:
+        Eij=0
+        for j in range(N):
+            Etemp=0
+            if j!=u:
+                dij=distance(r[u,0],r[j,0],r[u,1],r[j,1],r[u,2],r[j,2])
+                if dij<1:
+                    Etemp=1/math.pow(dij,3)
+                if dij<Lred/2:
+                    Etemp=1/math.pow(dij,2)*math.exp(-dij+1)
+            Eij=Eij+Etemp
+        Eij=Ared/N*Eij
+        
+        Eijnew=0
+        for j in range(N):
+            Etemp=0
+            if j!=u:
+                dij=distance(xnew,r[j,0],ynew,r[j,1],znew,r[j,2])
+                if dij<1:
+                    Etemp=1/math.pow(dij,3)
+                if dij<Lred/2:
+                    Etemp=1/math.pow(dij,2)*math.exp(-dij+1)
+            Eijnew=Eijnew+Etemp
+        Eijnew=Ared/N*Eijnew
+        
+        Enew=E-Eij+Eijnew
+        D_E=Eijnew-Eij
         print(D_E)
-        r[i,0]=xnew
-        r[i,1]=ynew
-        r[i,2]=znew
-        E=Enew
-        accep+=1
-    else:
-        rannum=ran.random()
-        if rannum<=math.exp(-D_E):
-            r[i,0]=xnew
-            r[i,1]=ynew
-            r[i,2]=znew
+        if D_E<=0:
+            r[u,0]=xnew
+            r[u,1]=ynew
+            r[u,2]=znew
             E=Enew
             accep+=1
-            
-   
-plt.plot(Ener)
+        else:
+            rannum=ran.random()
+            if rannum<=math.exp(-D_E):
+                r[u,0]=xnew
+                r[u,1]=ynew
+                r[u,2]=znew
+                E=Enew
+                accep+=1            
+       
+    plt.plot(Ener)
 
             
 
